88a1755d4165fc13f5dd2fa6f6402904481a8185
cupnoodles37 <roopre48@daum.net>
2025-02-21 22:38:10 +0900

    header 레이아웃 제작

--- src/components/Header/Nav.tsx
+++ src/components/Header/Nav.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 // import { menuData } from "../../constants/menuData";
 
 const Nav = (): React.JSX.Element => {
